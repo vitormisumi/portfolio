2ccf2f8853917e2acb72551866da8a5a1bb97c4f
Vitor Misumi <vitormisumi@gmail.com>
2024-08-27 15:37:17 -0300

    test: form

--- src/routes/[[lang=lang]]/contact/+page.svelte
+++ src/routes/[[lang=lang]]/contact/+page.svelte
@@ -1,1 +1,12 @@
+<script lang="ts">
+	import formbricks from '@formbricks/js/website';
+
+	if (typeof window !== 'undefined') {
+		formbricks.init({
+			environmentId: 'cm0cr8u1a0008e68q7901w2tw',
+			apiHost: 'https://formbricks-dwgc8wsk8okkko0ss084gsow.vitormisumi.com'
+		});
+	}
+</script>
+
 <h1>Contato</h1>
