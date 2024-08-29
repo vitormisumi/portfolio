93d7f0b598563c5e680059f8073fa6243f8e2e71
Vitor Misumi <vitormisumi@gmail.com>
2024-08-29 17:16:39 -0300

    feat: styling of about and projects sections

--- src/routes/[[lang=lang]]/about/+page.svelte
+++ src/routes/[[lang=lang]]/about/+page.svelte
@@ -1,31 +1,37 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
+
 	let { data } = $props();
 
 	let selectedSection = $state(1);
 </script>
 
 <div
-	class="fixed top-0 -z-10 grid h-screen w-full grid-cols-4 place-items-center content-center text-secondary landscape:flex-row">
-	<div>
+	class="fixed top-0 -z-10 grid h-screen w-full grid-cols-4 place-items-center text-secondary">
+	<div class="h-full max-h-96">
 		{#each data.about_sections as section}
 			<ul class="list-disc hover:text-accent">
-				<li class="flex items-center space-x-2">
-					<iconify-icon icon={section.section_icon}></iconify-icon>
-					<button onclick={() => (selectedSection = section.id)}
-						>{section.translations[0].section_title}</button>
+				<li>
+					<button onclick={() => (selectedSection = section.id)}>
+						{section.translations[0].section_title}
+					</button>
 				</li>
 			</ul>
 		{/each}
 	</div>
-	<div class="col-span-3">
+	<div class="col-span-3 h-full max-h-96">
 		{#each data.about_sections as section}
 			{#if section.id === selectedSection}
-				<h2 class="text-center">{section.translations[0].section_title}</h2>
-				<div class="grid max-h-96 grid-cols-2 overflow-y-scroll">
-					<p class="text-sm">{section.translations[0].section_text}</p>
-					<img
-						src="https://directus.vitormisumi.com/assets/{section.section_image}?width=400"
-						alt={section.translations[0].image_alt} />
+				<div in:fade>
+					<h2 class="font-roboto text-primary">{section.translations[0].section_title}</h2>
+					<div class="grid max-h-96 grid-cols-2 overflow-y-scroll">
+						<div>
+							{@html section.translations[0].section_text}
+						</div>
+						<img
+							src="https://directus.vitormisumi.com/assets/{section.section_image}?width=400"
+							alt={section.translations[0].image_alt} />
+					</div>
 				</div>
 			{/if}
 		{/each}
--- src/routes/[[lang=lang]]/projects/+page.svelte
+++ src/routes/[[lang=lang]]/projects/+page.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
 	let { data } = $props();
+
+	let selectedProject = $state(1);
 </script>
 
-<div class="text-secondary">
-	{#each data.projects as project}
-		<h2>{project.translations[0].title}</h2>
-		<p>{project.translations[0].description}</p>
-	{/each}
+<div
+	class="fixed top-0 -z-10 grid h-screen w-full grid-cols-4 place-items-center text-secondary">
+	<div class="h-full max-h-96">
+		{#each data.projects as project}
+			<button onclick={() => (selectedProject = project.id)}>
+				{project.translations[0].title}
+			</button>
+		{/each}
+	</div>
+	<div class="col-span-3 h-full max-h-96">
+		{#each data.projects as project}
+			{#if project.id === selectedProject}
+				<h2>{project.translations[0].title}</h2>
+				<p>{project.translations[0].description}</p>
+			{/if}
+		{/each}
+	</div>
 </div>
